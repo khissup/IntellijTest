f985915eeb76d15288b0fdd0e3f50f929286eae5
khissup <khissup@naver.com>
2022-04-04 23:30:37 +0900

    First Test

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Change Test");
+        // java 강의듣기전 임시 commit
     }
 }
